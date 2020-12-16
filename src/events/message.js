7a03b6c8723f31d9fc41225129e93d348fe0e8db
adryd <me@adryd.com>
2020-12-16 11:20:15 -0500

    Fix responding to all messages with ! prefix
    Prevents the bot from replying with "You do not have permission to use that command." for nonexistant commands.
    
    fix syntax issue

--- src/events/message.js
+++ src/events/message.js
@@ -6,16 +6,17 @@ warnable.client.on("message", msg => {
     if (!msg.author.bot && msg.guild) {
         if (config.guilds.hasOwnProperty(msg.guild.id)) {
             if (msg.content.startsWith(config.prefix)) {
-                if (config.guilds[msg.guild.id].roles.admin.some(r => msg.member.roles.cache.has(r))) {
-                    let command = msg.content.toLowerCase().split(" ")[0].substr(config.prefix.length);
-                    if (warnable.commands.hasOwnProperty(command)) warnable.commands[command](msg);
-                    console.info("[command]", `${msg.author.tag} > "${msg.content}"`);
-                }
-                else {
-                    msg.channel.send("", { embed: {
-                        color: warnable.config.msg.colorError,
-                        description: "You do not have permission to use that command."
-                    }});
+                let command = msg.content.toLowerCase().split(" ")[0].substr(config.prefix.length);
+                if (warnable.commands.hasOwnProperty(command)) { 
+                    if (config.guilds[msg.guild.id].roles.admin.some(r => msg.member.roles.cache.has(r))) {
+                        warnable.commands[command](msg);
+                        console.info("[command]", `${msg.author.tag} > "${msg.content}"`);
+                    } else {
+                        msg.channel.send("", { embed: {
+                            color: warnable.config.msg.colorError,
+                            description: "You do not have permission to use that command."
+                        }});
+                    }
                 }
             }
         }
