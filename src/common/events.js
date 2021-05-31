eefa7ad7084372c711502235c4f4f30e57c4eddd
Zachary <me@zachary.fun>
2021-06-01 00:30:09 +1000

    unshitcoded evnets! Thanks Ari! :)
    Co-Authored-By: adryd <mailbox_github.com@adryd.com>

--- src/common/events.js
+++ src/common/events.js
@@ -4,57 +4,43 @@
 const { client, commands, logs } = require('../warnable');
 const punishments = require('./punishments');
 
-client.on('ready', async () => {
-  if (!client.application.owner) await client.application.fetch();
-  logs.console('event', `Logged in and ready as '${client.user.tag}'`);
-  const cmdData = [];
+const events = ['ready', 'message', 'interaction', 'guildMemberAdd'];
+events.forEach((event) => {
+  client.on(event, (...args) => {
+    runEvent(event, args);
+  });
+});
 
-  for(const command in commands) {
-    try {
-      if (commands[command]['ready']) commands[command].ready();
-      if (commands[command]['meta']) {
-        cmdData.push(commands[command].meta);
-      }
+// Event Handler
+async function runEvent(event, args) {
+  // First event stuff...
+  if (event === 'ready') {
+    if (!client.application.owner) await client.application.fetch();
+    logs.console('event', `Logged in and ready as '${client.user.tag}'`);
+    const cmdData = [];
+    for(const command in commands) {
+      if (commands[command]['meta']) cmdData.push(commands[command].meta);
     }
-    catch(e) {
-      logs.console('error', `Event error on 'ready' by '${command}'`);
-    }
+
+    const appCommands = await client.application.commands.set(cmdData);
+    logs.console('command', `${appCommands.size} application commands applied! `);
+    appCommands.forEach((cmd) => {
+      logs.console('command', `Intention ID: ${cmd.id} | Name: ${cmd.name} (${cmd.description})`);
+    });
+
+    setInterval(() => {
+      Object.keys(process.servers).map((server) => {
+        punishments.check(server);
+      });
+    }, 10000);
   }
 
-  const appCommands = await client.application.commands.set(cmdData);
-  logs.console('command', `${appCommands.size} application commands applied! `);
-  appCommands.forEach((cmd) => {
-    logs.console('command', `Intention ID: ${cmd.id} | Name: ${cmd.name} (${cmd.description})`);
-  });
+  if (event === 'interaction') {
+    logs.console('command', `Interaction ${args[0].isCommand() ? `'${args[0].commandName}'` : ''} run by ${args[0].user.tag} (${args[0].user.id})`);
+  }
 
-  setInterval(() => {
-    Object.keys(process.servers).map((server) => {
-      punishments.check(server);
-    });
-  }, 10000);
-});
-
-client.on('message', (msg) => {
-  cmdEvent('message', (msg));
-});
-
-client.on('guildMemberAdd', (member) => {
-  punishments.rejoin(member.guild.id, member.user.id);
-  cmdEvent('guildMemberAdd', (member));
-});
-
-client.on('interaction', async interaction => {
-  cmdEvent('interaction', (interaction));
-  logs.console('event', `Interaction ${interaction.isCommand() ? `'${interaction.commandName}'` : ''} run by ${interaction.user.tag} (${interaction.user.id})`);
-});
-
-function cmdEvent(event, vals) {
+  // Command file events...
   for(const command in commands) {
-    try {
-      if (commands[command][event]) commands[command][event](vals);
-    }
-    catch(e) {
-      logs.console('error', `Event error on '${event}' by '${command}'`);
-    }
+    if (commands[command][event]) commands[command][event](...args);
   }
 }
