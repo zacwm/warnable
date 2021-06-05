18940844dedee553a47f48540a790d024bad05e1
Zachary <me@zachary.fun>
2021-06-05 13:00:51 +1000

    Invite automod now works by using client.fetchInvite for guild ID

--- src/modules/automod.js
+++ src/modules/automod.js
@@ -14,42 +14,67 @@ exports.message = function automod(msg) {
     // Discord Invites
     if (serverConfig.automod.invites.enabled) {
       const inviteCodes = msg.content.match(/(https?:\/\/)?(www.)?(discord.(gg|io|me|li)|discordapp.com\/invite)\/[^\s/]+?(?=\b)/gm);
-      const deleteMessage = inviteCodes.some(function(urls) {
+      /* const deleteMessage = inviteCodes.some(function(urls) {
         const parsedInvite = urls.replace(/(https?:\/\/)?(www.)?(discord.(gg|io|me|li)|discordapp.com\/invite)\//gm, '').split('/')[0];
         return !(serverConfig.automod.invites.excluded || []).includes(parsedInvite);
-      });
-      if (deleteMessage) {
-        msg.delete();
-        if (serverConfig.automod.invites.points > 0) {
-          const wGuildID = msg.guild.id;
-          const wUserID = msg.author.id;
-          const wPoints = serverConfig.automod.invites.points || 0;
-          const wIssuerID = client.user.id;
-          const wReason = `[warnable] Automod - Deleted invite in #${msg.channel.name}`;
-          const wTime = (new Date(new Date().toUTCString()).getTime() / 1000).toString();
-
-          db.addWarning(wGuildID, wUserID, wPoints, wIssuerID, wReason, wTime)
-          .then(async (v) => {
-            if (v) {
-              const newList = await db.listWarnings(wGuildID, wUserID);
-              const pointTotal = newList.reduce((prev, val) => prev + val.points, 0);
-              const descString = `**Warned:** <@${wUserID}> (${wUserID})`
-              + `\n**Points:** ${wPoints} point${wPoints !== 1 ? 's' : ''} (New total: ${newList.reduce((prev, val) => prev + val.points, 0) || '?'})`
-              + `\n**Reason:** \`${wReason}\``;
-
-              if (pointTotal) pointCheck(wGuildID, wUserID, pointTotal, wIssuerID);
-
-              logs.guild(msg.guild.id, 'main', {
-                title: 'Automod warning',
-                description: descString + `\n**Issuer:** <@${wIssuerID}>`,
-                color: 0xe67e22,
-              });
+      }); */
+      for (let i = 0; i < inviteCodes.length; i++) {
+        console.dir(inviteCodes[i]);
+        client.fetchInvite(inviteCodes[i])
+        .then((res) => {
+          if (res.guild) {
+            if (!serverConfig.automod.invites.excluded.includes(res.guild.id)) {
+              runAutomod(msg, 'invites', inviteCodes[i]);
             }
-          }).catch(vErr => {
-            console.error(vErr);
-          });
-        }
+          }
+          else {
+            runAutomod(msg, 'invites', 'Unknown');
+          }
+        })
+        .catch(() => {
+          runAutomod(msg, 'invites', 'Unknown');
+        });
       }
     }
   }
 };
+
+function runAutomod(msg, type, reasonExtras) {
+  msg.delete();
+  const automodTypeProps = process.servers[msg.guild.id].automod[type];
+  if (automodTypeProps.points > 0) {
+    const wGuildID = msg.guild.id;
+    const wUserID = msg.author.id;
+    const wPoints = automodTypeProps.points || 0;
+    const wIssuerID = client.user.id;
+    let wReason = '[warnable] Automod';
+    const wTime = (new Date(new Date().toUTCString()).getTime() / 1000).toString();
+
+    switch(type) {
+      case 'invites':
+        wReason = `[warnable] Automod - Discord invite (${reasonExtras}) in #${msg.channel.name}`;
+        break;
+    }
+
+    db.addWarning(wGuildID, wUserID, wPoints, wIssuerID, wReason, wTime)
+    .then(async (v) => {
+      if (v) {
+        const newList = await db.listWarnings(wGuildID, wUserID);
+        const pointTotal = newList.reduce((prev, val) => prev + val.points, 0);
+        const descString = `**Warned:** <@${wUserID}> (${wUserID})`
+        + `\n**Points:** ${wPoints} point${wPoints !== 1 ? 's' : ''} (New total: ${newList.reduce((prev, val) => prev + val.points, 0) || '?'})`
+        + `\n**Reason:** \`${wReason}\``;
+
+        if (pointTotal) pointCheck(wGuildID, wUserID, pointTotal, wIssuerID);
+
+        logs.guild(msg.guild.id, 'main', {
+          title: 'Automod warning',
+          description: descString + `\n**Issuer:** <@${wIssuerID}>`,
+          color: 0xe67e22,
+        });
+      }
+    }).catch(vErr => {
+      console.error(vErr);
+    });
+  }
+}
